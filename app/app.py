cc00293717bb66a16bb2e06225ffc70e82439dc0
Michael Herman <hermanmu@gmail.com>
2014-09-12 23:07:54 -0500

    dynamic app

--- app/app.py
+++ app/app.py
@@ -1,14 +1,24 @@
 # import flask
-from flask import Flask
+from flask import Flask, render_template, request
 
 # initilize flask
 app = Flask(__name__)
 
 
 # setup the route
-@app.route('/')
-def index():
+@app.route('/hello')
+def hello():
     return "Hello, World!"
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        value_one = int(request.form['number-one'])
+        value_two = int(request.form['number-two'])
+        total = value_one + value_two
+        return render_template('index.html', value=total)
+    return render_template('index.html')
 
 # run the server
 if __name__ == '__main__':
